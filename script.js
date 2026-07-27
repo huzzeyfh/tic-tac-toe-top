11bdfcc3d67c56eaf550adc1c198c07a96c73935
huzzeyfh <hudzaifah7103@gmail.com>
2026-07-27 21:51:08 +0700

    Add getValue() to make conditionWin works

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function Gameboard() {
+// Board
+const Gameboard = (() => {
   const rows = 3;
   const columns = 3;
   let board = [];
@@ -12,8 +13,8 @@ function Gameboard() {
 
   const getBoard = () => board;
 
-  const pointMark = (cellRow, cellCol, player) => {
-    board[cellRow][cellCol].addMark(player);
+  const pointMark = (cell, player) => {
+    cell.addMark(player);
   };
 
   const printBoard = () => {
@@ -22,8 +23,9 @@ function Gameboard() {
   };
 
   return { getBoard, pointMark, printBoard };
-}
+})();
 
+// Cell
 function Cell() {
   let value = 0;
 
@@ -33,18 +35,14 @@ function Cell() {
 
   const getValue = () => value;
 
-  return {
-    addMark,
-    getValue,
-  };
+  return { addMark, getValue };
 }
 
+// Controller
 function GameController(
   playerOneName = "Player One",
   playerTwoName = "Player Two",
 ) {
-  const board = Gameboard();
-
   const players = [
     {
       name: playerOneName,
@@ -58,32 +56,78 @@ function GameController(
 
   let activePlayer = players[0];
 
+  const board = Gameboard.getBoard();
+
   const switchTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
   const getActivePlayer = () => activePlayer;
 
   const printNewRound = () => {
-    board.printBoard();
+    Gameboard.printBoard();
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
+  const conditionWin = () => {
+    board[0][0].getValue() === "X" &&
+    board[0][1].getValue() === "X" &&
+    board[0][2].getValue() === "X"
+      ? console.log("Win Condition met!")
+      : console.log("Not Met");
+  };
+
   const playRound = (cellRow, cellCol) => {
+    const chosenCell = board[cellRow][cellCol];
     console.log(
       `Adding ${getActivePlayer().name}'s mark at Cell Row: ${cellRow} and Cell Column: ${cellCol}...`,
     );
-    board.pointMark(cellRow, cellCol,  getActivePlayer().mark);
+    Gameboard.pointMark(chosenCell, getActivePlayer().mark);
 
+    conditionWin();
     switchTurn();
     printNewRound();
   };
 
   printNewRound();
 
-  return {
-    playRound,
-    getActivePlayer,
-  };
+  return { playRound, getActivePlayer };
 }
 
 const game = GameController();
+
+// //Kertas coretan
+//   //first try
+// const conditionWin = Gameboard.getBoard().filter(
+//       (row) => row[0].getValue() === players.mark,
+//     );
+
+//     if (conditionWin === true) {
+//       console.log("Win Condition met!")
+//     };
+
+//   //second try
+// const rows = 3;
+// const columns = 3;
+// let board = [];
+
+// for (let i = 0; i < rows; i++) {
+//   board[i] = [];
+//   for (let j = 0; j < columns; j++) {
+//     board[i].push(0);
+//   }
+// }
+// board[0][0] = "X";
+// board[0][1] = "X";
+// board[0][2] = "X";
+
+// console.log(board);
+
+// // const conditionWin = board.filter((row) => row[0] === "X");
+
+// // if (conditionWin === true) {
+// //   console.log("Win Condition met!");
+// // }
+
+// function conditionWin() {
+//   board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X" ? console.log("Win Condition met!") : console.log("Not Met");
+// }
