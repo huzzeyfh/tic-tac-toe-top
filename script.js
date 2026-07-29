265abda9df04d85c1b530b3e22de3d71ee868b5d
huzzeyfh <hudzaifah7103@gmail.com>
2026-07-29 16:56:09 +0700

    Make the game logic works first on console and then built the DOM UI

--- script.js
+++ script.js
@@ -13,16 +13,115 @@ const Gameboard = (() => {
 
   const getBoard = () => board;
 
+  const getBoardValues = () =>
+    board.map((row) => row.map((cell) => cell.getValue()));
+
+  const winningLines = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ], // topRow
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ], // midRow
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ], // botRow
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ], // firstCol
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ], // secondCol
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ], // thirdCol
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ], // diag1
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ], // diag2
+  ];
+
+  const getWinningLines = () => winningLines;
+  const getWinningLinesValues = () =>
+    winningLines.map((line) =>
+      line.map(([row, col]) => getBoardValues()[row][col]),
+    );
+
   const pointMark = (cell, player) => {
     cell.addMark(player);
   };
 
   const printBoard = () => {
-    const boardTTC = board.map((row) => row.map((cell) => cell.getValue()));
-    console.log(boardTTC);
+    console.log(getBoardValues());
   };
 
-  return { getBoard, pointMark, printBoard };
+  const conditionWin = () => {
+    const winningLinesValues = getWinningLinesValues();
+
+    const isWinningLineTrue = winningLinesValues.some(
+      (line) => line[0] === line[1] && line[1] === line[2] && line[0] !== 0,
+    );
+
+    if (isWinningLineTrue) {
+      console.log("Win Condition met!");
+      return true;
+    } else {
+      console.log("Win Condition not met.");
+      return false;
+    }
+  };
+
+  const conditionDraw = () => {
+    const boardValues = getBoardValues();
+    const flatValues = boardValues.flat();
+
+    const isBoardFull = flatValues.every((value) => value !== 0);
+
+    if (isBoardFull) {
+      return true;
+    } else {
+      console.log("Draw Condition not met.");
+      return false;
+    }
+  };
+
+  const resetBoard = () => {
+    board = [];
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(Cell());
+      }
+    }
+    console.log("Board reset.");
+  };
+
+  return {
+    getBoard,
+    pointMark,
+    printBoard,
+    conditionWin,
+    conditionDraw,
+    resetBoard,
+  };
 })();
 
 // Cell
@@ -56,11 +155,10 @@ function GameController(
 
   let activePlayer = players[0];
 
-  const board = Gameboard.getBoard();
-
   const switchTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
+
   const getActivePlayer = () => activePlayer;
 
   const printNewRound = () => {
@@ -68,24 +166,36 @@ function GameController(
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
-  const conditionWin = () => {
-    board[0][0].getValue() === "X" &&
-    board[0][1].getValue() === "X" &&
-    board[0][2].getValue() === "X"
-      ? console.log("Win Condition met!")
-      : console.log("Not Met");
-  };
-
   const playRound = (cellRow, cellCol) => {
-    const chosenCell = board[cellRow][cellCol];
-    console.log(
-      `Adding ${getActivePlayer().name}'s mark at Cell Row: ${cellRow} and Cell Column: ${cellCol}...`,
-    );
-    Gameboard.pointMark(chosenCell, getActivePlayer().mark);
+    const chosenCell = Gameboard.getBoard()[cellRow][cellCol];
 
-    conditionWin();
-    switchTurn();
-    printNewRound();
+    if (chosenCell.getValue() !== 0) {
+      return console.log("Cell already marked. Choose another cell.");
+    } else {
+      console.log(
+        `Adding ${getActivePlayer().name}'s mark at Cell Row: ${cellRow} and Cell Column: ${cellCol}...`,
+      );
+      Gameboard.pointMark(chosenCell, getActivePlayer().mark);
+
+      if (Gameboard.conditionWin() === true) {
+        console.log(`${getActivePlayer().name} wins!`);
+        Gameboard.printBoard();
+        Gameboard.resetBoard();
+        console.log("Starting a new game...");
+        return printNewRound();
+      }
+
+      if (Gameboard.conditionDraw() === true) {
+        console.log("It's a draw!");
+        Gameboard.printBoard();
+        Gameboard.resetBoard();
+        console.log("Starting a new game...");
+        return printNewRound();
+      }
+
+      switchTurn();
+      printNewRound();
+    }
   };
 
   printNewRound();
@@ -93,7 +203,51 @@ function GameController(
   return { playRound, getActivePlayer };
 }
 
-const game = GameController();
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.createElement("div");
+  playerTurnDiv.id = "turn";
+  document.body.appendChild(playerTurnDiv);
+
+  const boardDiv = document.createElement("div");
+  boardDiv.id = "board";
+  document.body.appendChild(boardDiv);
+
+  const updateScreen = () => {
+    boardDiv.textContent = "";
+
+    const board = Gameboard.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn.`;
+
+    board.forEach((row, cellRow) => {
+      row.forEach((cell, cellCol) => {
+          const cellButton = document.createElement("button");
+          cellButton.classList.add("cell");
+          cellButton.dataset.row = cellRow;
+          cellButton.dataset.col = cellCol;
+          cellButton.textContent = cell.getValue();
+          boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  function clickHandlerBoard(e) {
+    const selectedRow = e.target.dataset.row;
+    const selectedCol = e.target.dataset.col;
+
+    if (!selectedRow || !selectedCol) return;
+
+    game.playRound(selectedRow, selectedCol);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  updateScreen();
+}
+
+ScreenController();
 
 // //Kertas coretan
 //   //first try
